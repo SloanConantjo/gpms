4130bcd96e71b64d2e0ff8bd55d8ed7c73d318d1
Earendil <914386539@qq.com>
2023-05-30 18:39:24 +0800

    admin sql change

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const connection = require("../mysql/sql");
 
 exports.adminAccount = function(req, res) {
-    connection.c.query('SELECT * FROM `account` LIMIT 100', function(err, results, fields){
+    connection.query('SELECT * FROM `account` LIMIT 100', function(err, results, fields){
         res.render('adminAccount', {title: 'Account List', error: err, data: results});
     });
 }

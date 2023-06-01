93ed76345a6a63510a55d54f34bd486692002a3b
Earendil <914386539@qq.com>
2023-06-01 22:08:18 +0800

    student paper

--- routes/student.js
+++ routes/student.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 let stu = require('../controllers/student');
+const multer = require('multer')
+
+//文档上传
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, './public/documents');
+    },
+    filename(req, file, cb) {
+      cb(null, req.params.id + file.originalname);
+    }
+  })
+const upload = multer({ storage });
 
 /* GET home page. */
 router.get('/', stu.stuHome);
@@ -16,7 +28,13 @@ router.get('/topic/select/:id', stu.stuTopicSelectGet);
 
 router.post('/topic/select/:id', stu.stuTopicSelectPost);
 
-router.get('/paper', stu.stuPaper);
+router.get('/paper', stu.stuPaperList);
+
+router.get('/paper/:id', stu.stuPaper);
+
+router.post('/paper/new', stu.stuPaperNew);
+
+router.post('/paper/:id', upload.single('file'), stu.stuPaperUpload);
 
 router.get('/profile', stu.stuProfile);
 
